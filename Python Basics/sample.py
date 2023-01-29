8ef066483ef1a660f6b648b7d3cdf446188e6ef5
arpitdoshi88 <arpit54@gmail.com>
2023-01-29 22:39:56 +0530

    test

--- Python Basics/sample.py
+++ Python Basics/sample.py
@@ -3,3 +3,11 @@ cities = ["Delhi","Tanzania","New York"]
 
 for i in zip(countries,cities):
     print(i)
+
+print()
+
+for count,country in enumerate(countries):
+    print(count,country)
+
+a= "Hello"
+print(a.swapcase())
